0505c697ec7af5f0d2668b7500829b2d31e92b8a
barunrai01 <barun_rai01@live.com>
2024-09-18 14:41:38 -0700

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,46 +1,17 @@
-"""
-Emotion Detection Server
+import requests
 
-This script defines a Flask-based server for performing emotion detection on user-provided text.
-
-Author(Learner): [NoorAldin]
-"""
-
-from flask import Flask, render_template, request
-from EmotionDetection.emotion_detection import emotion_detector
-from EmotionDetection.emotion_detection import emotion_predictor
-
-app = Flask("Emotion Detection")
-
-def run_emotion_detection():
-    """
-    Main function to run the Emotion Detection application.
-    """
-    app.run(host="0.0.0.0", port=5000)
-
-@app.route("/emotionDetector")
-def sent_detector():
-    """
-    Analyze the user-provided text for emotions and return the result.
-    """
-    text_to_detect = request.args.get('textToAnalyze')
-    response = emotion_detector(text_to_detect)
-    formated_response = emotion_predictor(response)
-    if formated_response['dominant_emotion'] is None:
-        return "Invalid text! Please try again."
-    return (
-        f"For the given statement, the system response is 'anger': {formated_response['anger']} "
-        f"'disgust': {formated_response['disgust']}, 'fear': {formated_response['fear']}, "
-        f"'joy': {formated_response['joy']} and 'sadness': {formated_response['sadness']}. "
-        f"The dominant emotion is {formated_response['dominant_emotion']}."
-    )
-
-@app.route("/")
-def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    run_emotion_detection()
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+    
+    response = requests.post(url, headers=headers, json=input_json)
+    
+    # Return the response text attribute, if available
+    return response.json().get('text', None)
